5cc76cb2a5adaac36f515ab5dbbb535c402f9e1a
eonsubae <eonsubae@naver.com>
2020-07-09 10:58:49 +0900

    SSR Debugging

--- pages/_app.js
+++ pages/_app.js
@@ -7,14 +7,24 @@ import Navbar from '@/components/shared/Navbar';
 import Hero from '@/components/shared/Hero';
 
 const MyApp = ({ Component, pageProps }) => {
+  const isHomePage = () => Component.name === 'Home';
+
   return (
     <div className="portfolio-app">
       <Navbar />
-      {pageProps.appData}
-      {Component.name === 'Home' ? <Hero /> : null}
+      {isHomePage() && <Hero />}
       <div className="container">
         <Component {...pageProps} />
       </div>
+      {/* FOOTER STARTS */}
+      {isHomePage() && (
+        <footer id="sticky-footer" className="py-4 bg-black text-white-50 py-3">
+          <div className="container text-center">
+            <small>Copyright &copy; Your Website</small>
+          </div>
+        </footer>
+      )}
+      {/* FOOTER ENDS */}
     </div>
   );
 };
@@ -25,14 +35,14 @@ const MyApp = ({ Component, pageProps }) => {
   이 기능을 이용하고 싶다면 전체 컴포넌트에 적용되는 _app.js의 getInitialProps를 사용하지 않아야 한다
 */
 
-// MyApp.getInitialProps = async (context) => {
-//   console.log('GET INITIAL PROPS _APP');
-//   const initialProps =
-//     App.getInitialProps && (await App.getInitialProps(context));
+MyApp.getInitialProps = async (context) => {
+  console.log('GET INITIAL PROPS _APP');
+  const initialProps =
+    App.getInitialProps && (await App.getInitialProps(context));
 
-//   return {
-//     pageProps: { appData: 'Hello _App Component', ...initialProps.pageProps },
-//   };
-// };
+  return {
+    pageProps: { appData: 'Hello _App Component', ...initialProps.pageProps },
+  };
+};
 
 export default MyApp;
