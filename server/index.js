9c15e5212300f640a6702684d74e9fa4b715b64f
eonsubae <eonsubae@naver.com>
2020-07-10 15:00:31 +0900

    Portfolio resolvers

--- server/index.js
+++ server/index.js
@@ -8,21 +8,61 @@ const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
+const data = {
+  portfolios: [
+    {
+      _id: '32das7d89',
+      title: 'Job in USA',
+      content: 'It was very nice experience',
+      jobTitle: 'Chef',
+      daysOfExperience: 100,
+      isCurrentlyEmployed: false,
+    },
+    {
+      _id: '21qw1c31e123',
+      title: 'Job in Barcelona',
+      content: 'It was very sunny experience',
+      jobTitle: 'Developer',
+      daysOfExperience: 133,
+      isCurrentlyEmployed: true,
+    },
+    {
+      _id: '1bwc11456',
+      title: 'Job in Germany',
+      content: 'It was very good experience',
+      jobTitle: 'Manager',
+      daysOfExperience: 30,
+      isCurrentlyEmployed: true,
+    },
+  ],
+};
+
 app.prepare().then(() => {
   const server = express();
 
   // Construct a schema, using GraphQL schema language
   const schema = buildSchema(`
+    type Portfolio {
+      _id: ID!
+      title: String
+      content: String
+      jobTitle: String,
+      daysOfExperience: Int,
+      isCurrentlyEmployed: Boolean
+    }
+
     type Query {
       hello: String
+      portfolio: Portfolio
+      portfolios: [Portfolio]
     }
   `);
 
   // The root provides a resolver for each API endpoint
   const root = {
-    hello: () => {
-      return 'Hello World';
-    },
+    hello: () => 'Hello World',
+    portfolio: () => data.portfolios[0],
+    portfolios: () => data.portfolios,
   };
 
   server.use(
