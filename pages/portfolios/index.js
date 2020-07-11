86a5d3d991e06099c1d1a7fec2e86e04a0dfd754
eonsubae <eonsubae@naver.com>
2020-07-11 14:54:08 +0900

    Create portfolio

--- pages/portfolios/index.js
+++ pages/portfolios/index.js
@@ -1,6 +1,40 @@
-import axios from 'axios';
-import PortfolioCard from '@/components/portfolios/PortfolioCard';
+import { useState } from 'react';
 import Link from 'next/link';
+import axios from 'axios';
+
+import PortfolioCard from '@/components/portfolios/PortfolioCard';
+
+const graphCreatePortfolio = () => {
+  const query = `
+  mutation CreatePortfolio {
+    createPortfolio(input: {
+      title: "New title"
+      company: "New company"
+      companyWebsite: "New website"
+      location: "New location"
+      jobTitle: "New jobtitle"
+      description: "New Description"
+      startDate: "12/12/2012"
+      endDate: "14/11/2013"
+    }) {
+      _id,
+      title,
+      company,
+      companyWebsite,
+      location,
+      jobTitle,
+      description,
+      startDate,
+      endDate
+    }
+  }
+  `;
+
+  return axios
+    .post('http://localhost:3000/graphql', { query })
+    .then(({ data: graph }) => graph.data)
+    .then((data) => data.createPortfolio);
+};
 
 const fetchPortfolios = () => {
   const query = `
@@ -26,7 +60,15 @@ const fetchPortfolios = () => {
     .then((portfolios) => portfolios);
 };
 
-const Portfolios = ({ portfolios }) => {
+const Portfolios = ({ data }) => {
+  const [portfolios, setPortfolios] = useState(data.portfolios);
+
+  const createPortfolio = async () => {
+    const newPortfolio = await graphCreatePortfolio();
+    const newPortfolios = [...portfolios, newPortfolio];
+    setPortfolios(newPortfolios);
+  };
+
   return (
     <>
       <section className="section-title">
@@ -35,6 +77,9 @@ const Portfolios = ({ portfolios }) => {
             <h1>Portfolios</h1>
           </div>
         </div>
+        <button onClick={createPortfolio} className="btn btn-primary">
+          Create Portfolio
+        </button>
       </section>
       <section className="pb-5">
         <div className="row">
@@ -55,7 +100,7 @@ const Portfolios = ({ portfolios }) => {
 
 Portfolios.getInitialProps = async () => {
   const portfolios = await fetchPortfolios();
-  return { portfolios };
+  return { data: { portfolios } };
 };
 
 export default Portfolios;
