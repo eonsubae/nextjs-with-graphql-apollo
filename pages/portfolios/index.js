bd27d79188dbd31ff650269e954d46d772795fc8
eonsubae <eonsubae@naver.com>
2020-07-12 22:55:37 +0900

    Create portfolio cache

--- pages/portfolios/index.js
+++ pages/portfolios/index.js
@@ -54,15 +54,34 @@ const graphUpdatePortfolio = (id) => {
 const Portfolios = () => {
   const [portfolios, setPortfolios] = useState([]);
   const [getPortfolios, { loading, data }] = useLazyQuery(GET_PORTFOLIOS);
-  const [createPortfolio, { data: createdData }] = useMutation(
-    CREATE_PORTFOLIO
-  );
+  const [createPortfolio] = useMutation(CREATE_PORTFOLIO, {
+    update(cache, { data: { createPortfolio } }) {
+      const { portfolios } = cache.readQuery({ query: GET_PORTFOLIOS });
+      cache.writeQuery({
+        query: GET_PORTFOLIOS,
+        data: { portfolios: [...portfolios, createPortfolio] },
+      });
+    },
+  });
+
+  /*
+  const onPortfolioCreated = (createdData) =>
+    setPortfolios([...portfolios, createdData.createPortfolio]);
+
+  const [createPortfolio] = useMutation(CREATE_PORTFOLIO, {
+    onCompleted: onPortfolioCreated,
+  });
+  */
 
   useEffect(() => {
     getPortfolios();
   }, []);
 
-  if (data && data.portfolios.length > 0 && portfolios.length === 0) {
+  if (
+    data &&
+    data.portfolios.length > 0 &&
+    (portfolios.length === 0 || data.portfolios.length !== portfolios.length)
+  ) {
     setPortfolios(data.portfolios);
   }
 
