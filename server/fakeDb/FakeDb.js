b8e20091b6442d8346dd1dc2d5f5a7151c39c0dd
eonsubae <eonsubae@naver.com>
2020-07-16 22:27:27 +0900

    Populate DB

--- server/fakeDb/FakeDb.js
+++ server/fakeDb/FakeDb.js
@@ -1,7 +1,19 @@
-class FakeDb {
-  populate() {}
+const { portfolios } = require('./data');
+const Portfolio = require('../database/models/portfolio');
 
-  clean() {}
+class FakeDb {
+  async clean() {
+    await Portfolio.deleteMany({});
+  }
+
+  async addData() {
+    await Portfolio.create(portfolios);
+  }
+
+  async populate() {
+    await this.clean();
+    await this.addData();
+  }
 }
 
 module.exports = new FakeDb();
